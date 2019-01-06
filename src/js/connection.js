6a00cda41af035bd5d5b48bd2b136554eae1ea51
pablinho <pablinho.blanco@gmail.com>
2019-01-06 14:17:52 +0100

    dependencias de yarn web3

--- src/js/connection.js
+++ src/js/connection.js
@@ -2,92 +2,98 @@ var connection = {
   web3: false,
   contract: false,
   contractInstance: false,
-
+  currentAccount: false,
   start: function() {
     var that = this;
 
     that.web3 = new Web3(new Web3.providers.HttpProvider( conf.httpProvider ));
-  	that.contract = web3.eth.contract( conf.abi );
-  	that.contractInstance = gGisContract.at( conf.contractAddress );
+  	that.contract = that.web3.eth.contract( conf.abi );
+  	that.contractInstance = that.contract.at( conf.contractAddress );
+    that.currentAccount = that.contract.eth.accounts[0];
+
+
+    //console.log(that.contractInstance.setAccount.sendTransaction('lol', 'lolazo Descripción', true, 10,10,4, {from:that.contract.eth.accounts[0], gas:200000}));
+    //console.log(that.contractInstance.accountExist.call('lal').toString());
+
+
   },
 
-    /*
-      Contract functions call
-    */
-    setAccount: function(  data, onComplete, gasLimit ) {
-      var that = this;
-      that.contractInstance.setAccount.sendTransaction(
-        data.accountIdName,
-        data.desc,
-        data.clusterize,
-        data.lat,
-        data.lng,
-        data.zoom,
-        {
-          from: conf.account,
-          gas:gasLimit
-        },
-        function( r ){
-          onComplete( r );
-        }
-      );
-    },
+  /*
+    Contract functions call
+  */
+  setAccount: function(  data, onComplete, gasLimit ) {
+    var that = this;
 
-    getAccount: function( data, onComplete  ) {
-      var that = this;
 
-      that.contractInstance.getAccount.call(
-        data.accountIdName,
-        function( r ){
-          onComplete( r );
-        }
-      );
-    }
+    that.contractInstance.setAccount.sendTransaction(
+      data.accountIdName,
+      data.desc,
+      data.clusterize,
+      data.lat,
+      data.lng,
+      data.zoom,
+      {
+        from: that.currentAccount,
+        gas: gasLimit
+      },
+      function(err, res){ onComplete(err,res)}
+    );
 
-    setPlace: function( data, onComplete, gasLimit ) {
-      var that = this;
+    //that.contractInstance.setAccount.sendTransaction('lal', 'lolazo Descripción', true, 10,10,4, {from:conf.account, gas:200000})
+  },
 
-      that.contractInstance.setPlace.sendTransaction(
-        data.accountIdName,
-        data.ttIndex,
-        data.title,
-        data.desc,
-        data.imageLink,
-        data.lat,
-        data.lng,
-        data.zoom,
-        {
-          from: conf.account,
-          gas:gasLimit
-        },
-        function( r ){
-          onComplete( r );
-        }
-      );
-    },
+  getAccount: function( data, onComplete  ) {
+    var that = this;
+    that.contractInstance.getAccount.call(
+      data.accountIdName,
+      function(err, res) { onComplete( err, res ) }
+    );
+  },
 
-    getPlace: function( data, onComplete ) {
-      var that = this;
+  setPlace: function( data, onComplete, gasLimit ) {
+    var that = this;
 
-      that.contractInstance.getPlace.call(
-        data.accountIdName,
-        data. ttIndex,
-        function( r ){
-          onComplete( r );
-        }
-      );
-    },
+    that.contractInstance.setPlace.sendTransaction(
+      data.accountIdName,
+      data.ttIndex,
+      data.title,
+      data.desc,
+      data.imageLink,
+      data.lat,
+      data.lng,
+      data.zoom,
+      {
+        from: that.currentAccount,
+        gas:gasLimit
+      },
+      function( err, res ){
+        onComplete( err, res );
+      }
+    );
+  },
 
-    listPlaceIndex: function() {
-        var that = this;
+  getPlace: function( data, onComplete ) {
+    var that = this;
 
-        that.contractInstance.getPlace.call(
-          data.accountIdName,
-          function( r ){
-            onComplete( r );
-          }
-        );
-    }
+    that.contractInstance.getPlace.call(
+      data.accountIdName,
+      data. ttIndex,
+      function( r ){
+        onComplete( err, res );
+      }
+    );
+  },
+
+  listPlaceIndex: function(data, onComplete) {
+    var that = this;
+
+    that.contractInstance.listPlaceIndex.call(
+      data.accountIdName,
+      function( err, res ){
+        onComplete( err, res );
+      }
+    );
+  }
 
 
 };
