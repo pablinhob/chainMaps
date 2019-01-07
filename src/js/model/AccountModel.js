202f995192fd8baf844927b814940e4cc5d11ec8
pablinho <pablinho.blanco@gmail.com>
2019-01-07 21:11:50 +0100

    modelo account

--- src/js/model/AccountModel.js
+++ src/js/model/AccountModel.js
@@ -1,3 +1,5 @@
+const GPS_PRECISSION = 1000000000;
+
 var AccountModel = Backbone.Model.extend({
   defaults: {
     accountIdName: false,
@@ -6,14 +8,47 @@ var AccountModel = Backbone.Model.extend({
     lat: false,
     lng: false,
     zoom: false,
+    placesNumber: false
   },
 
   saveOnContract: function() {
+    var that = this;
 
+    var accountData = that.toJSON();
+    accountData.lat = that.fromLatLngToInt(ccountData.lat);
+    accountData.lng = that.fromLatLngToInt(ccountData.lng);
+
+    contract.setAccount(
+      accountData,
+      function( err, res ) {
+      },
+      200000
+    );
   },
 
   loadFromContract: function() {
-    
+    var that = this;
+    contract.getAccount(
+      that.toJSON(),
+      function( err, res ) {
+        that.set( 'accountIdName',res[0] );
+        that.set( 'desc',res[0] );
+        that.set( 'clusterize',res[0] );
+        that.set( 'lat', that.fromIntToLatLng(res[0]) );
+        that.set( 'lng', that.fromIntToLatLng(res[0]) );
+        that.set( 'zoom',res[0] );
+      }
+    );
+  },
+
+  fromLatLngToInt: function( val ){
+    var that = this;
+    return parseInt(val*GPS_PRECISSION);
+  },
+
+  fromIntToLatLng: function( val ){
+    var that = this;
+    return parseFloat(val/GPS_PRECISSION);
   }
 
 });
