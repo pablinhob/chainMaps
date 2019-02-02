398faa87032a76af329b48d1b7320ffef1f9cd76
pablinho <pablinho.blanco@gmail.com>
2019-02-02 17:09:49 +0100

    account balance

--- src/js/ethAccount.js
+++ src/js/ethAccount.js
@@ -1,9 +1,13 @@
 var ethAccount = {
-  privateKey: '15d7f910f479c3e2f0d2eee48a71ab4f017cc786ca31122f37b3518f44b27bfb',
+  //privateKey: '15d7f910f479c3e2f0d2eee48a71ab4f017cc786ca31122f37b3518f44b27bfb',
+  privateKey: false,
   getPublicKey: function() {
     var that = this;
     var b = ethereumjs.Util.toBuffer('0x'+that.privateKey);
     var wallet = ethereumjs.Wallet.fromPrivateKey( b );
     return ethereumjs.Util.bufferToHex( wallet.getAddress() );
+  },
+  getBalance: function( onResultFunction ) { // balance in wei
+    contract.web3.eth.getBalance(ethAccount.getPublicKey() ).then(function(d){ onResultFunction(d) } );
   }
 }
